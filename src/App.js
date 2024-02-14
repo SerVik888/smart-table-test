a5fb99a201362e20f77cf600b767bbdeea63be53
Сафонов Сергей <genom.86@inbox.ru>
2024-02-14 11:12:57 +0300

    Update Readme.md.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,8 @@ import Table from './Table/Table';
 const App = () => {
   return (
     <div className="appWrapper">
-            {/* <SearchBar filterTable={props.filterTable}/> */}
-            <Table
-            // data={props.data}
-            //        sortMethod={props.sortMethod}
-            //        reverseToggler={props.reverseToggler}
-            //        searchQuery={props.searchQuery}
-            //        sortTable={props.sortTable}
-            //        toggleModal={props.toggleModal}
-            //        getData={props.getData}
-                   />
-        </div>
+      <Table />
+    </div>
   );
 }
 
